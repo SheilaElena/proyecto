cb808f52570b267fd73db44eaa163eeca414ac21
SheilaElena <coachingslsants@gmail.com>
2025-03-12 00:11:50 +0100

    Delete JS directory

--- JS/AceptaC.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('form').addEventListener('submit', function (event) {
-        const checkbox = document.getElementById('terminos');
-        if (!checkbox.checked) {
-            alert('Debes aceptar los términos de la página antes de continuar.');
-            event.preventDefault(); // Evita el envío del formulario
-        }
-    });
-});
