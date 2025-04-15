1f5915b5a8d9f81fca61b580c9a86d68d53ef866
SheilaElena <coachingslsants@gmail.com>
2025-04-15 20:39:37 +0200

    Update campana.php

--- resultados-web/php/campana.php
+++ resultados-web/php/campana.php
@@ -1,9 +1,8 @@
-<!-- CONEXION -->
 <?php
+ob_start();
+session_start();
 
-    session_start();
-
-      $servername = "mysql-db";
+    $servername = "mysql-db";
     $username = "sea";
     $password = "proyectose@";
     $database = "bd_keyloggers";
